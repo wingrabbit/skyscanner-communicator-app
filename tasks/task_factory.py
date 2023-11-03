231153d4bddf7c87cd31ebcc410c7f206b4b449b
Ilya Epshteyn <i.a.epshtein@gmail.com>
2023-11-03 01:07:52 -0400

    format code

--- tasks/task_factory.py
+++ tasks/task_factory.py
@@ -2,13 +2,17 @@ import datetime
 from tasks.global_task import GlobalTask
 from tasks.task import Task
 
+
 def generate_tasks(global_task: GlobalTask) -> [Task]:
     result = []
-    start_date = datetime.datetime.strptime(global_task.dates_range[0], '%Y-%m-%d').date()
-    end_date = datetime.datetime.strptime(global_task.dates_range[1], '%Y-%m-%d').date()
+    start_date = datetime.datetime.strptime(
+        global_task.dates_range[0], '%Y-%m-%d').date()
+    end_date = datetime.datetime.strptime(
+        global_task.dates_range[1], '%Y-%m-%d').date()
     curr = start_date
-    while curr<=end_date:
+    while curr <= end_date:
         for i in range(global_task.days_range[0], global_task.days_range[1]+1):
-            result.append(Task(global_task.origin, global_task.destination, curr.strftime('%Y-%m-%d'), (curr+datetime.timedelta(days=i)).strftime('%Y-%m-%d')))
-        curr+=datetime.timedelta(days=1)
+            result.append(Task(global_task.origin, global_task.destination, curr.strftime(
+                '%Y-%m-%d'), (curr+datetime.timedelta(days=i)).strftime('%Y-%m-%d')))
+        curr += datetime.timedelta(days=1)
     return result
