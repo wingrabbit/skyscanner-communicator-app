31e0755fae089f15ec30a2576db52ec361dc2639
Ilya Epshteyn <i.a.epshtein@gmail.com>
2024-01-16 19:06:48 -0500

    implement file path param

--- writers/file_writer.py
+++ writers/file_writer.py
@@ -1,19 +1,20 @@
 from writers.base_writer import BaseWriter
+from config import result_files_path
 
 class FileWriter(BaseWriter):
 
     def __init__(self, path) -> None:
-        self.path = path
+        self.path = f"{result_files_path}{path}.txt"
     
     def write_list(self, data):
-        f = open("search_results/test.txt", 'a+')
+        f = open(self.path, 'a+')
         for i in data:
             f.write(str(i))
             f.write('\n')
         f.close()
     
     def write_line(self, data):
-        f = open("search_results/test.txt", 'a+')
+        f = open(self.path, 'a+')
         f.write(str(data))
         f.write('\n')
         f.close()
