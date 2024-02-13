a7ca897a81082d1e3e2cf40a285895b3fe529137
Ilya Epshteyn <i.a.epshtein@gmail.com>
2024-02-13 00:02:50 -0500

    wait 1 second after getting a city from the API

--- dao_communication/dao_city.py
+++ dao_communication/dao_city.py
@@ -1,4 +1,5 @@
 import requests
+import time
 from config import DAO_server_URL, DAO_city_URL
 from models.destination import Destination
 from queries.search_destination import SearchDestination
@@ -11,6 +12,7 @@ def get_city(city_name) -> Destination:
         destination = get_destination(SearchDestination(city_name).execute())
         city_json = {"name": destination.title, "sky_id": destination.sky_id, "entity_id": destination.entity_id}
         attempt = requests.post(url, json=city_json)
+        time.sleep(1)
     result_json = attempt.json()
     result = Destination(id=result_json["id"], entity_id=result_json["entity_id"], title=result_json["name"], sky_id=result_json["sky_id"])
     return result
